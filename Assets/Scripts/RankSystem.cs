609ea561133c45434f12fcceb8985587751eb9a4
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-12 20:01:54 +0300

    Rank system works :smiley_cat:

---
+++ Assets/Scripts/RankSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankSystem : MonoBehaviour
+{
+    public List<GameObject> players = new List<GameObject> (); //elemt 0 is our player ;)
+    public Text rankText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindRank();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindRank();
+    }
+
+    void FindRank()
+    {
+        int currentRank = 1;
+        for(int count = 1; count < players.Count; count++)
+        {
+            if (players[0].transform.position.z <= players[count].transform.position.z)
+                currentRank++;
+        }
+        rankText.text = "Rank: " + currentRank.ToString();
+    }
+}
